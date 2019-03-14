ff47653c3271af36bc8c4781523623128cb08964
jawehner <jawehner@gmail.com>
2019-03-14 15:54:04 -0400

    View

---
+++ NotesApp.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,20 @@
+﻿using NotesApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotesApp.WebMVC.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+    }
+}
