baa37bb0750d3a87064699dc63d2e600b3ad2547
jawehner <jawehner@gmail.com>
2019-04-24 14:34:48 -0400

    updated create view

--- NotesApp.Models/NoteCreate.cs
+++ NotesApp.Models/NoteCreate.cs
@@ -10,9 +10,16 @@ namespace NotesApp.Models
     public class NoteCreate
     {
         [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
         public string Title { get; set; }
+
+        [MaxLength(8000)]
         public string Content { get; set; }
 
-        public override string ToString() => Title;
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
